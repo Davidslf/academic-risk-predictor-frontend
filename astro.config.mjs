450859005d77e6e81a730e79deafebf51f294ad8
Angie C <angievanesasanchez10i3@gmail.com>
2025-11-17 23:56:18 -0500

    Se cambia el endpoint por el de render

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://predictor-academico.vercel.app",  // Actualiza con tu dominio
     output: 'static',
-    adapter: vercel(),
     // Configuración de build
     build: {
         inlineStylesheets: 'auto',
